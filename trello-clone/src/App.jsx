bd026559202c5ef1b3684dc67ee453c5449fc62e
Ashok M A <ashok5angel@gmail.com>
2019-09-15 23:39:44 +0200

    feature: Create new board :added

--- trello-clone/src/App.jsx
+++ trello-clone/src/App.jsx
@@ -7,6 +7,8 @@ class App extends React.Component {
   constructor() {
     super();
     this.state = {
+      newBoard: false,
+      newBoardTitle: '',
       boards: [
         {
           title: 'AshKeys',
@@ -23,20 +25,76 @@ class App extends React.Component {
     };
   }
 
-  componentDidMount() {
-    sessionStorage.setItem(
-      TrelloCloneConstants.BOARDS,
-      JSON.stringify(this.state.boards)
-    );
+  componentWillMount() {
+    !sessionStorage.getItem(TrelloCloneConstants.BOARDS) &&
+      sessionStorage.setItem(
+        TrelloCloneConstants.BOARDS,
+        JSON.stringify(this.state.boards)
+      );
   }
+
+  toggleNewBoard = () => {
+    this.setState({
+      newBoard: !this.state.newBoard
+    });
+  };
+
+  addNewBoard = (e) => {
+    e.preventDefault();
+    const boards = [
+      ...this.state.boards,
+      { title: this.state.newBoardTitle, todo: [], inProgress: [], done: [] }
+    ];
+    this.setState({
+      boards,
+      newBoardTitle: ''
+    });
+    sessionStorage.setItem(TrelloCloneConstants.BOARDS, JSON.stringify(boards));
+
+    this.render();
+  };
+
+  onTitleEnter = ({ target: { value } }) =>
+    this.setState({
+      newBoardTitle: value
+    });
 
   render() {
     return (
       <div className="App">
-        <div className="board-new-item">
-          <h3 className="board-new-item__title"> Create new board...</h3>
+        <div
+          hidden={this.state.newBoard}
+          className="board-new-tile"
+          onClick={this.toggleNewBoard}
+        >
+          <h3 className="board-new-tile__title">Create a new board...</h3>
         </div>
-        <BoardList />
+        <div className="board-new-tile" hidden={!this.state.newBoard}>
+          <h3 className="board-new-tile__title">Creating a new board...</h3>
+          <form
+            className="board-new-form"
+            noValidate
+            onSubmit={this.addNewBoard}
+            onReset={this.toggleNewBoard}
+          >
+            <input
+              className="board-new-form__input"
+              type="text"
+              name="title"
+              placeholder="Please enter a title..."
+              value={this.state.newBoardTitle}
+              onChange={this.onTitleEnter}
+              required
+            />
+            <button className="board-new-form__button--reset" type="reset">
+              Cancel
+            </button>
+            <button className="board-new-form__button--submit" type="submit">
+              Create
+            </button>
+          </form>
+        </div>
+        <BoardList boards={this.state.boards} />
       </div>
     );
   }
--- trello-clone/src/board-list/index.jsx
+++ trello-clone/src/board-list/index.jsx
@@ -4,12 +4,11 @@ import { TrelloCloneConstants } from '../util';
 import './board-list.scss';
 
 export class BoardList extends React.Component {
-  constructor() {
-    super();
-    this.state = {
+  componentWillMount() {
+    this.setState({
       boards:
         JSON.parse(sessionStorage.getItem(TrelloCloneConstants.BOARDS)) || []
-    };
+    });
   }
 
   render() {
@@ -19,8 +18,8 @@ export class BoardList extends React.Component {
         <div className="boardlist">
           {this.state.boards.map((board, index) => (
             <Link to={'/boards/' + index}>
-              <div className="boardlist-item">
-                <h3 className="boardlist-item__title">{board.title}</h3>
+              <div className="boardlist-tile">
+                <h3 className="boardlist-tile__title">{board.title}</h3>
               </div>
             </Link>
           ))}
